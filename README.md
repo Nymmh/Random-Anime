46da7ff43b5b70cca1db300a0535ca7eb524e419
Nymmh <stickman7415web@gmail.com>
2016-10-14 18:26:57 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Random-Anime
 Random anime chrome extension 
+
+
+Version 0.5.0
+Initial release 
+
+Version 1.0.0
+Fixed the problem with having to right click open new tab
--- js/background.js
+++ js/background.js
@@ -1,1 +1,4 @@
-var storage = chrome.storage.local;
+chrome.browserAction.onClicked.addListener(function(activeTab){
+	var newURL = "https://www.google.ca/";
+	chrome.tabs.create({ url: newURL });
+});
